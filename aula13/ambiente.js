ee4bb35b5814bece8f0fa93d4cb58351a84076c5
Lincoln-g20 <andrelincolnfs2014@gmail.com>
2024-08-30 14:38:16 -0300

    atualização de estudos

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+
+console.log ("vai começar!")
+for (var c=1; c<=10; c++){
+    console.log (`Passo ${c}`)
+}
+console.log ("Terminou.")
+
+
+/* var c = 0
+while(c<=50) {console.log(`Passo ${c}`)
+     c++
+    
+} */
+
+/*var c = 0
+do {console.log(`Passo ${c}`)
+    c++
+    
+} while (c<=50) */
+
+/*console.log ("tudo bem?")
+console.log ("tudo bem?")
+console.log ("tudo bem?")
+console.log ("tudo bem?")
+console.log ("tudo bem?")
+console.log ("tudo bem?")*/
